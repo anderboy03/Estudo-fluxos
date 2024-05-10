0337383a79dab1d66c32160ef6f4b990380efbda
Anderfoxx <anderfox1988@gmail.com>
2024-05-09 21:39:17 -0300

    commit ternario com if e else if

--- fluxo-trabalho-java/src/resultadoTernario.java
+++ fluxo-trabalho-java/src/resultadoTernario.java
@@ -1,8 +1,8 @@
 public class resultadoTernario {
 
     public static void main(String[] args) {
-        int nota =7;
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        int nota =4;
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 
         System.out.println(resultado);
     }
