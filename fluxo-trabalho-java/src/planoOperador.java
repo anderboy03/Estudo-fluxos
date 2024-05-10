a08aa4f7c655363bb47b690dc8f34c37446fc016
Anderfoxx <anderfox1988@gmail.com>
2024-05-09 22:10:03 -0300

    commit if else case

---
+++ fluxo-trabalho-java/src/planoOperador.java
@@ -0,0 +1,23 @@
+public class planoOperador {
+
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if(plano == "B"){
+        System.out.println("100 minutos de ligações");
+        }else if(plano ==  "M"){
+            System.out.println("100 minutos de ligaçoes");
+            System.out.println("whats e instagram gratis");
+        }else if(plano == "T"){
+            System.out.println("100 minutos de ligaçoes");
+            System.out.println("whats e instagram gratis");
+            System.out.println("5GB youtube");
+        }
+
+
+
+
+
+    }
+    
+}
