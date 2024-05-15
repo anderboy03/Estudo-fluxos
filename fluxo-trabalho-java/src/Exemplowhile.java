8b12cb2030a39e4443e6d196189045cfa2c95f8e
Anderfoxx <anderfox1988@gmail.com>
2024-05-15 19:26:44 -0300

    commit while

--- fluxo-trabalho-java/src/Exemplowhile.java
+++ fluxo-trabalho-java/src/Exemplowhile.java
@@ -1,19 +1,25 @@
+// ExemploWhile.java
 import java.util.concurrent.ThreadLocalRandom;
-
 public class Exemplowhile {
-    public static void main(String[] args) {
-        double mesada= 50.0;
-        while(mesada>0){
+public static void main(String[] args) {
+	double mesada = 50.0;
+        while(mesada>0) {
             Double valorDoce = valorAleatorio();
-            if(valorDoce>mesada)
-            valorDoce = mesada;
+            if(valorDoce > mesada)
+                valorDoce = mesada;
 
-            System.out.println("doce do valor:" + valorDoce "adicionando no carrinho mesada:" . mesada=mesada - valorDoce);
-            System.out.println("joao gastou todo sua mesada em doces");
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+            mesada = mesada - valorDoce;
         }
-        private static double valorAleatorio(){
-            return ThreadLocalRandom.current().nextDouble(2,8);
-        }
-    }
-    
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+        
+        /*
+        * Não se preocupe quanto a formatação de valores
+        * Iremos explorar os recursos de formatação em breve !!
+        */
+   }
+   private static double valorAleatorio() {
+	return ThreadLocalRandom.current().nextDouble(2, 8);
+   }
 }
