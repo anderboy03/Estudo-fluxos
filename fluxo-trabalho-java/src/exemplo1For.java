2d60d652868decfee118b1dc68f34bd7ec320c1f
Anderfoxx <anderfox1988@gmail.com>
2024-05-15 16:46:51 -0300

    commit array com for

---
+++ fluxo-trabalho-java/src/exemplo1For.java
@@ -0,0 +1,10 @@
+public class exemplo1For {
+    public static void main(String[] args) {
+        String alunos[]= {"Felipe","jonas","Julia","Marcos"};
+        for (int x=0; x<alunos.length; x++ ){
+        System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+
+    }
+    
+}
